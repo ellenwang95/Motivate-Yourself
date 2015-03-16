b642a35ef6956fcb1a02629170639f6f9784a724
ellenwang95 <ellenwang95@yahoo.ca>
2015-03-15 21:11:59 -0400

    Clickable slideshow instead of automatic rotation

--- popup.js
+++ popup.js
@@ -1,16 +1,19 @@
 var curImg = 2;
-var imagesArray = new Array();
-
 
 function rotateImages() {
 	document.getElementById("slideshow").setAttribute("src", "images/"+curImg+".jpg");
 	curImg = (curImg < 20) ? curImg+1 : 1;
 }
 
+function handler() {
+	rotateImages();
+}
+
 function main() {
-	setInterval("rotateImages()", 4000);
+	document.addEventListener('DOMContentLoaded', function() {
+		document.getElementById("slideshow").addEventListener("click", handler);
+	});
 }
 
 window.onload = main();
-
 
